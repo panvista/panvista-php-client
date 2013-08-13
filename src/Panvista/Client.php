60f62ba6dd2a2149f8ca5b7ff02fdf7cc2cbc346
Ben Maynard <bmaynard@panvistamobile.com>
2013-08-13 12:58:49 -0400

    Use cacert.pem mozilla.org - http://curl.haxx.se/docs/caextract.html

--- src/Panvista/Client.php
+++ src/Panvista/Client.php
@@ -176,6 +176,7 @@ class Client
         curl_setopt($ch, CURLOPT_TIMEOUT, 60);
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
         curl_setopt($ch, CURLOPT_URL, $requestUrl);
+        curl_setopt($ch, CURLOPT_CAINFO, dirname(__FILE__) . '/cacert.pem');
 
         if (!empty($data)) {
             curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
